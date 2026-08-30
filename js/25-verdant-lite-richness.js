e81fa6511308d081a8f8cceac66bb8e9b2eac94d
clinicbot <jonmidi@gmail.com>
2026-08-30 12:31:10 +0300

    Bump Verdant release label to v126

--- js/25-verdant-lite-richness.js
+++ js/25-verdant-lite-richness.js
@@ -72,7 +72,7 @@
     return w;
   };
 
-  const RELEASE='125';
+  const RELEASE='126';
   const label=()=>{
     const b=document.getElementById('buildTag');
     if(b)b.textContent='build '+RELEASE;
