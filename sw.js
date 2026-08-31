89916499d41b27f355361b3b5e91937ef496a367
clinicbot <jonmidi@gmail.com>
2026-08-31 12:12:24 +0300

    Cache Aqua v151 coral and jelly layer

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ const CORE = [
   'js/51-aqua-fish-visibility-v145.js', 'js/52-aqua-depth-distribution-v146.js',
   'js/53-aqua-swim-motion-v147.js', 'js/54-aqua-tail-animation-v148.js',
   'js/55-aqua-uturn-continuity-v149.js', 'js/56-aqua-faces-reef-v150.js',
-  'js/28-verdant-instanced-renderer.js',
+  'js/57-aqua-coral-jelly-v151.js', 'js/28-verdant-instanced-renderer.js',
   'assets/images/verdant_rift_card.svg', 'assets/images/aqua_rift_card.svg', 'assets/images/sky_verdant.svg',
   'assets/models/verdant_bear.gltf', 'assets/models/verdant_frog.gltf',
   'assets/models/verdant_monkey.gltf', 'assets/models/verdant_ship.gltf',
